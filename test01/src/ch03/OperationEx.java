4cbc2c6061bc4399d7528c3dc4549b6f7d2a1731
DESKTOP-R4K628H\it <realisshobeen@gmail.com>
2025-01-03 17:14:14 +0900

    파일 추가

---
+++ test01/src/ch03/OperationEx.java
@@ -0,0 +1,23 @@
+package ch03;
+
+public class OperationEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int i=2;
+		
+		boolean value =((num1=num1+10)<10) && ((i=i+2)<10);
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i); //단락회로평가 : 앞의 항이 falses면 뒤 항의 결과를 평가하지 않아도 false
+		
+		value=((num1=num1+10)>10)||((i=i+2)<10);
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i); // 앞의 항이 true면 뒤 항의 결과를 평가하지 않아도 true임
+		
+		
+	}
+
+}
